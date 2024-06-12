917550f0231d6f2120ae78a68bddd61cefb9cd15
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-06-12 20:51:24 +0300

    Macro

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[macro_export]
+macro_rules! first_and_last {
+    ($input:expr) => {{
+        let mut parts = $input.split_whitespace();
+        let first = parts.next().unwrap_or_default();
+        let last = parts.last().unwrap_or_default();
+        (first, last)
+    }};
 }
