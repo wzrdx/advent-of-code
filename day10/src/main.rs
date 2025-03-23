16ee5e63e2dc3b73e9d578d62d4d71678b03e5b8
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-03-23 19:46:13 +0200

    Day 10 - Parse cycles

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,59 +1,57 @@
 extern crate common;
+
 use common::{first_and_last, read_lines};
 
 // Day #10
 pub fn main() {
     let lines: Vec<String> = read_lines("day10/input.txt");
 
-    let mut x: i32 = 1;
-    let mut cycle: i32 = 0;
-    let mut stack = vec![20, 60, 100, 140, 180, 220];
-    let mut values: Vec<i32> = Vec::new();
+    // let mut x: u16 = 1;
+    let mut cycle: u8 = 1;
+    let mut index: u16 = 0;
 
-    lines.iter().for_each(|line| {
-        let (instr, arg) = first_and_last!(line);
+    let array: Vec<i16> = lines
+        .iter()
+        .map(|line| {
+            let (instr, arg) = first_and_last!(line);
 
-        if stack.is_empty() {
-            return;
-        }
-
-        if instr == "addx" {
-            let value = arg.parse::<i32>().expect("Failed to parse number.");
-
-            if cycle + 2 >= stack[0] {
-                println!(
-                    "[{:?}] ({:?}) Value for {:?} is {:?}",
-                    cycle,
-                    x,
-                    stack[0],
-                    x * stack[0]
-                );
-
-                values.push(x * stack[0]);
-                stack.remove(0);
+            if instr == "addx" {
+                let value = arg.parse::<i16>().expect("Failed to parse number.");
+                value
+            } else {
+                0
             }
+        })
+        .collect();
 
-            cycle += 2;
-            x += value;
-            // println!("[{:?}] ({:?}) {:?}, {:?}", cycle, x, instr, value);
+    let mut queue: Option<i16> = None;
+
+    while index < array.len() as u16 {
+        let value = array[index as usize];
+
+        if queue.is_none() {
+            let instr_str = if value == 0 {
+                "noop".to_string()
+            } else {
+                format!("addx {}", value)
+            };
+
+            println!("[{:?}] Starting: {:?}", cycle, instr_str);
+
+            index += 1;
         } else {
-            if cycle + 1 >= stack[0] {
-                println!(
-                    "[{:?}] ({:?}) Value for {:?} is {:?}",
-                    cycle,
-                    x,
-                    stack[0],
-                    x * stack[0]
-                );
-
-                values.push(x * stack[0]);
-                stack.remove(0);
-            }
-
-            cycle += 1;
-            // println!("[{:?}] ({:?}) {:?}", cycle, x, instr);
+            println!(
+                "[{:?}] Finished: {:?}",
+                cycle,
+                format!("addx {}", queue.unwrap())
+            );
+            queue = None;
         }
-    });
 
-    println!("Sum: {}", values.iter().sum::<i32>());
+        if value != 0 {
+            queue = Some(value);
+        }
+
+        cycle += 1;
+    }
 }
