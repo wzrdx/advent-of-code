e090f8fbb91a9332800ef6edadcf4eb17071f83a
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-04-09 23:20:30 +0300

    Day 15 - Testing

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -13,8 +13,10 @@ struct Point {
 struct Sensor {
     coverage: i32,
     coords: Point,
-    // beacon: Point,
 }
+
+const MAX: i32 = 4_000_000;
+const MULTIPLIER: i32 = 4_000_000;
 
 // Day #15
 pub fn main() {
@@ -42,20 +44,30 @@ pub fn main() {
             Sensor {
                 coverage: get_distance(&coords, &beacon),
                 coords,
-                // beacon,
             }
         })
         .collect();
 
-    let coverage = get_row_coverage(2000000, &sensors) - 1; // Remove the beacon
-    println!("{:?}", coverage);
+    let mut index: i32 = 0;
+
+    while index <= 0 {
+        println!("{}", index);
+
+        if let Some(x_position) = get_possible_position(index, &sensors) {
+            println!("X: {}, Y: {}", x_position, index);
+            println!("Tuning Frequency: {}", x_position * MULTIPLIER + index);
+            break;
+        } else {
+            index += 1;
+        }
+    }
 }
 
 fn get_distance(a: &Point, b: &Point) -> i32 {
     ((a.x - b.x).abs() + (a.y - b.y).abs()) as i32
 }
 
-fn get_row_coverage(row: i32, sensors: &Vec<Sensor>) -> usize {
+fn get_possible_position(row: i32, sensors: &Vec<Sensor>) -> Option<i32> {
     let mut set: HashSet<i32> = HashSet::new();
 
     sensors
@@ -64,11 +76,23 @@ fn get_row_coverage(row: i32, sensors: &Vec<Sensor>) -> usize {
         .for_each(|s| {
             let diff = (s.coords.y - row).abs();
             let size = s.coverage - diff;
-            let start = s.coords.x - size;
-            let end = s.coords.x + size;
 
-            set.extend(start..=end);
+            let start = (s.coords.x - size).max(0);
+            let end = (s.coords.x + size).min(MAX);
+
+            println!("[{:?}] {} to {}", s.coords, start, end);
+
+            // set.extend(start..=end);
         });
 
-    set.len()
+    // if (set.len() as i32) == MAX {
+    //     // Find the missing position
+    //     for i in 0..=MAX {
+    //         if !set.contains(&i) {
+    //             return Some(i);
+    //         }
+    //     }
+    // }
+
+    None
 }
