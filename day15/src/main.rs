0a4f5644a548b91306a38733561fdeadf185f11f
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-04-09 23:52:42 +0300

    Day 15 - Part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use common::read_lines;
 use regex::Regex;
 
@@ -10,13 +8,19 @@ struct Point {
 }
 
 #[derive(Debug, Clone)]
+struct Range {
+    start: i32,
+    end: i32,
+}
+
+#[derive(Debug, Clone)]
 struct Sensor {
     coverage: i32,
     coords: Point,
 }
 
 const MAX: i32 = 4_000_000;
-const MULTIPLIER: i32 = 4_000_000;
+const MULTIPLIER: u64 = 4_000_000;
 
 // Day #15
 pub fn main() {
@@ -50,12 +54,13 @@ pub fn main() {
 
     let mut index: i32 = 0;
 
-    while index <= 0 {
-        println!("{}", index);
-
+    while index <= MAX {
         if let Some(x_position) = get_possible_position(index, &sensors) {
             println!("X: {}, Y: {}", x_position, index);
-            println!("Tuning Frequency: {}", x_position * MULTIPLIER + index);
+            println!(
+                "Tuning Frequency: {}",
+                (x_position as u64) * MULTIPLIER + (index as u64)
+            );
             break;
         } else {
             index += 1;
@@ -68,31 +73,33 @@ fn get_distance(a: &Point, b: &Point) -> i32 {
 }
 
 fn get_possible_position(row: i32, sensors: &Vec<Sensor>) -> Option<i32> {
-    let mut set: HashSet<i32> = HashSet::new();
-
-    sensors
+    let mut ranges = sensors
         .iter()
         .filter(|s| s.coords.y + s.coverage >= row && s.coords.y - s.coverage <= row)
-        .for_each(|s| {
+        .map(|s| {
             let diff = (s.coords.y - row).abs();
             let size = s.coverage - diff;
 
             let start = (s.coords.x - size).max(0);
             let end = (s.coords.x + size).min(MAX);
 
-            println!("[{:?}] {} to {}", s.coords, start, end);
+            Range { start, end }
+        })
+        .collect::<Vec<Range>>();
 
-            // set.extend(start..=end);
-        });
+    ranges.sort_by(|a, b| a.start.cmp(&b.start).then(a.end.cmp(&b.end)));
 
-    // if (set.len() as i32) == MAX {
-    //     // Find the missing position
-    //     for i in 0..=MAX {
-    //         if !set.contains(&i) {
-    //             return Some(i);
-    //         }
-    //     }
-    // }
+    let mut coverage = Range { start: 0, end: 0 };
+
+    for range in &ranges {
+        if range.start <= coverage.end && range.end > coverage.end {
+            coverage.end = range.end;
+        }
+    }
+
+    if coverage.end != MAX {
+        return Some(coverage.end + 1);
+    }
 
     None
 }
